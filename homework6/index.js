71cfe3ff708d35ce6d699cc2aad2447f6e5bf1bd
Ilona <>
2022-08-28 21:16:20 +0200

    no message

--- homework6/index.js
+++ homework6/index.js
@@ -2,17 +2,15 @@ const hero = ['Ivan'],
     native = ['York','Of'],
     destination = ['Poltava','In'];
 
-let rainbow = [...hero, ...native, ...destination].reverse(),
-    rainbowPop = rainbow.pop(),
-    rainbowSplice = rainbow.splice(4, 0, `Gave`, `Battle`),
-    rainbowMuve = rainbow.shift();
-    rainbow = rainbow.concat(rainbowMuve),
-    rainbowShift = rainbow.shift(),
-    rainbowUnshift = rainbow.unshift(`Richard`),
-    rainbowPush = rainbow.push(`Vain`);
-console.log(rainbow);
+let rainbow = [...hero, ...native, ...destination].reverse();
+    rainbow.pop();
+    rainbow.splice(4, 0, `Gave`, `Battle`);
+    rainbow = rainbow.concat(rainbow.shift());
+    rainbow.shift();
+    rainbow.unshift(`Richard`);
+    rainbow.push(`Vain`);
 
-for(let i = 0; i<rainbow.length; i++) {
+for(let i = 0; i < rainbow.length; i++) {
     let colors;
     switch (rainbow[i]) {
         case `Richard`:
@@ -37,8 +35,7 @@ for(let i = 0; i<rainbow.length; i++) {
             colors = `purple`;
             break;
     }
-    document.write(`<div style="font-family: 'Quicksand', sans-serif; display: inline-block; text-align: center;
-    padding: 10px;">
+    document.write(`<div style="font-family: 'Quicksand', sans-serif; display: inline-block; text-align: center;padding: 10px;">
                 <div style= "margin-bottom: 20px; border-radius: 50%; width: 50px; height: 50px; background-color: ${colors};"></div>
                 <span>${rainbow[i]}</span>
     </div>`)
