f896b9a43b28cf33f0050f445088f66114ab2a2e
Ilona <>
2022-08-28 13:18:13 +0200

    no message

---
+++ homework6/index.js
@@ -0,0 +1,45 @@
+const hero = ['Ivan'],
+    native = ['York','Of'],
+    destination = ['Poltava','In'];
+
+let rainbow = [...hero, ...native, ...destination].reverse(),
+    rainbowPop = rainbow.pop(),
+    rainbowSplice = rainbow.splice(4, 0, `Gave`, `Battle`),
+    rainbowMuve = rainbow.shift();
+    rainbow = rainbow.concat(rainbowMuve),
+    rainbowShift = rainbow.shift(),
+    rainbowUnshift = rainbow.unshift(`Richard`),
+    rainbowPush = rainbow.push(`Vain`);
+console.log(rainbow);
+
+for(let i = 0; i<rainbow.length; i++) {
+    let colors;
+    switch (rainbow[i]) {
+        case `Richard`:
+            colors = `red`;
+            break;
+        case `Of`:
+            colors = `orange`;
+            break;
+        case `York`:
+            colors = `yellow`;
+            break;
+        case `Gave`:
+            colors = `green`;
+            break;
+        case `Battle`:
+            colors = `blue`;
+            break;
+        case `In`:
+            colors = `indigo`;
+            break;
+        case `Vain`:
+            colors = `purple`;
+            break;
+    }
+    document.write(`<div style="font-family: 'Quicksand', sans-serif; display: inline-block; text-align: center;
+    padding: 10px;">
+                <div style= "margin-bottom: 20px; border-radius: 50%; width: 50px; height: 50px; background-color: ${colors};"></div>
+                <span>${rainbow[i]}</span>
+    </div>`)
+}
