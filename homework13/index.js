d1bed82c579a666069d108bd6d117449c9764b32
isivashkina <ilona4211@gmail.com>
2022-09-19 12:36:29 +0200

    no message

---
+++ homework13/index.js
@@ -0,0 +1,26 @@
+class Bulka {
+    constructor(name,size,ingredients){
+    this.name = name;
+    this.size = size;
+    this.ingredients = [`cutlet`, `salada`, `tomato`];
+    }
+    setAdditionalIngredients(...ingredient){
+        this.ingredients.push(...ingredient);
+        } 
+}
+
+let Humburger = new Bulka(`Humburger`, `small`);
+Humburger.setAdditionalIngredients(`egg`, `onion`);
+
+class Cheeseburger extends Bulka{
+    constructor(name,size,ingredients){
+        super(name,size,ingredients);
+        this.ingredients.push('cheese');
+    }
+}
+
+let myCheeseburger = new Cheeseburger(`Cheeseburger`, `small`);
+myCheeseburger.setAdditionalIngredients(`egg`, `onion`);
+
+console.log(Humburger);
+console.log(myCheeseburger);
