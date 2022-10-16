4f49752d508f3537d02369f89976e29adaa1429a
Ilona <>
2022-10-16 15:03:11 +0200

    no message

---
+++ homework17/index.js
@@ -0,0 +1,28 @@
+const renderElement = document.querySelector(`#renderElement`);
+const figure = document.querySelector(`#figure`);
+const color = document.querySelector(`#color`);
+const element = document.querySelector(`#element`);
+console.log(figure.value);
+
+let render = {
+    figure: ``,
+    figureLast: ``,
+    color: ``
+}
+
+renderElement.addEventListener(`submit`, e => {
+    e.preventDefault();
+    render.figureLast = render.figure;
+    render.figure = figure.value;
+    render.color = color.value;
+    renderForm(render);
+})
+
+function renderForm(render) {
+    if (render.figureLast.length) {
+        element.classList.remove(render.figureLast);
+    }
+    element.style.background = render.color;
+    element.classList.add(render.figure);
+    
+}
